17e1d6bd03b9e844ee8e6fe103d3d6ca543247b5
dung111999 <52844170+dung111999@users.noreply.github.com>
2021-04-25 10:15:25 +0700

    Update FetchData.js

--- src/function/FetchData.js
+++ src/function/FetchData.js
@@ -1,7 +1,7 @@
 export const FetchData = {
     async data() {
         var result = [];
-        await fetch('https://gist.githubusercontent.com/dung111999/4bc71dab72d71d411f3454d3efe32e88/raw/a569511fb0e7ed362f4a4b3201ddd545e1a7d6a8/gistfile1.txt')
+        await fetch('https://gist.githubusercontent.com/dung111999/4bc71dab72d71d411f3454d3efe32e88/raw/0feb1d516f012d681c78e57ca5e99715adc73284/gistfile1.txt')
             .then((response) => response.json())
             .then((responseJSON) => {
                 result = responseJSON
